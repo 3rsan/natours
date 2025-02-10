67c3bc3a1c9fd663ddf51ac92c19b90c70b94b7d
Murat Adıyaman <muratadiyaman0@gmail.com>
2025-02-11 00:24:33 +0300

    replace bug fixed

--- server.js
+++ server.js
@@ -10,10 +10,14 @@ process.on('uncaughtException', err => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB =
-  process.env.DATABASE !== undefined
-    ? process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
-    : '';
+let DB;
+
+if (process.env.DATABASE !== undefined) {
+  DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+  );
+}
 
 mongoose.connect(DB).then(() => console.log('DB connection successfull!'));
 
