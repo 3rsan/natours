7d90a7519a7c156084f7b45f55d2da8b64a3c499
Murat Adıyaman <muratadiyaman0@gmail.com>
2025-02-11 00:05:31 +0300

    replace controlled

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -11,6 +11,7 @@ class APIFeatures {
 
     // 1B) ADVANCE FILTERING
     let queryStr = JSON.stringify(queryObj);
+    if (queryStr === undefined) return;
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
     this.query.find(JSON.parse(queryStr));
